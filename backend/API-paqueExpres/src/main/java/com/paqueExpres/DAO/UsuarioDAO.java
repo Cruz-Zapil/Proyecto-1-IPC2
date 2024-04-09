14c1a911b482921db8e414613c6bd7b9b258f7af
Cruz-Zapil <victorzapil202030851@cunoc.edu.gt>
2024-04-09 13:16:15 -0600

    agregando: servles put y post, actualizando DAO

--- backend/API-paqueExpres/src/main/java/com/paqueExpres/DAO/UsuarioDAO.java
+++ backend/API-paqueExpres/src/main/java/com/paqueExpres/DAO/UsuarioDAO.java
@@ -1,5 +1,7 @@
 package com.paqueExpres.DAO;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,6 +15,7 @@ public class UsuarioDAO {
     private Connection jdbcConexion;
     private JSONArray listaClientes;
     private ConexionDb conexion;
+    private final String nameTable = "usuario";
 
     public void conectar() throws SQLException {
 
@@ -24,27 +27,38 @@ public class UsuarioDAO {
     }
 
     public JSONArray obtenerTodosLosUsuarios() throws SQLException {
-        String sqlScript = "SELECT * FROM usuario";
+        String sqlScript = "SELECT * FROM " + nameTable;
         listaClientes = new JSONArray();
         try (PreparedStatement statement = jdbcConexion.prepareStatement(sqlScript);
                 ResultSet resultSet = statement.executeQuery()) {
+
             // Obtener la lista de usuarios y retornarla
             listaClientes = listarUsuarios(resultSet);
+
+            // cerrando recursos
+            statement.close();
+            resultSet.close();
         }
         return listaClientes;
     }
 
     public JSONArray obtenerUsuarioEspecifico(String columna, String condicion) throws SQLException {
-        String sql = "SELECT * FROM usuario WHERE ? = ?";
+        String sql = "SELECT * FROM " + nameTable + " WHERE ? = ?";
         listaClientes = new JSONArray();
         try (PreparedStatement statement = jdbcConexion.prepareStatement(sql)) {
             statement.setString(1, columna);
             statement.setString(2, condicion);
+
             try (ResultSet resultSet = statement.executeQuery()) {
 
                 // Obtener la lista de usuarios y retornarla
                 listaClientes = listarUsuarios(resultSet);
+
+                // cerrando recursos
+                statement.close();
+                resultSet.close();
             }
+
         } finally {
             try {
                 // Cerrar la conexión con la base de datos
@@ -56,6 +70,102 @@ public class UsuarioDAO {
         return listaClientes;
     }
 
+    public JSONObject loggin(String username, String password) throws SQLException {
+
+        String sql = "SELECT * FROM " + nameTable + " WHERE username = ? AND password_has = ? ";
+        JSONObject user = new JSONObject();
+
+        try (PreparedStatement statement = jdbcConexion.prepareStatement(sql)) {
+
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+
+                /// acceder a los resultados
+                if (resultSet.next()) {
+
+                    user.put("ID", resultSet.getString("id_usuario"));
+                    user.put("Nombre", resultSet.getString("nombre"));
+                    user.put("Apellido", resultSet.getString("apellido"));
+                    user.put("Estado", resultSet.getString("estado"));
+                    user.put("ID rol", resultSet.getString("id_rol"));
+                    user.put("Teléfono", resultSet.getString("telefono"));
+                    user.put("Edad", resultSet.getString("edad"));
+
+                } else {
+
+                    // Si no se encontraron resultados
+                    user.put("verifique sus datos", "Usuario no encontrado");
+                }
+
+                // cerrando recursos
+                statement.close();
+                resultSet.close();
+
+            }
+
+        } finally {
+            try {
+
+                conexion.cerrarConexion();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return user;
+
+    }
+
+    /// update usuario
+
+    public boolean updateUser(BufferedReader datosEviados) throws IOException, SQLException {
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while ((line = datosEviados.readLine()) != null) {
+            sb.append(line);
+        }
+
+        JSONObject jsonData = new JSONObject(sb.toString());
+
+        String sql = "UPDATE " + nameTable
+                + " SET nombre = ?, apellido = ?, estado=?, id_rol=?, genero=?, telefono=?, edad=? WHERE id_usuario =? ";
+
+        try (PreparedStatement statement = jdbcConexion.prepareStatement(sql)) {
+
+            statement.setString(1, jsonData.getString("nombre"));
+            statement.setString(2, jsonData.getString("apellido"));
+            statement.setString(3, jsonData.getString("estado"));
+            statement.setString(4, jsonData.getString("id_rol"));
+            statement.setString(5, jsonData.getString("genero"));
+            statement.setString(6, jsonData.getString("telefono"));
+            statement.setString(7, jsonData.getString("edad"));
+            statement.setString(8, jsonData.getString("id_usuario"));
+
+            int filasAfectads = statement.executeUpdate();
+
+            // cerrando recursos
+            statement.close();
+
+            if (filasAfectads > 0) {
+                return true;
+            }
+
+        } finally {
+            try {
+
+                conexion.cerrarConexion();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return false;
+    }
+
     private JSONArray listarUsuarios(ResultSet resultSet) throws SQLException {
         // Crear un JSONArray para almacenar los usuarios
         JSONArray listaClientes = new JSONArray();
@@ -64,20 +174,19 @@ public class UsuarioDAO {
             // Crear un JSONObject para cada usuario y añadirlo al JSONArray
             JSONObject usuario = new JSONObject();
 
-            usuario.put("id", resultSet.getString("id_usuario"));
-            usuario.put("nombre", resultSet.getString("nombre"));
-            usuario.put("apellido", resultSet.getString("apellido"));
-            usuario.put("username", resultSet.getString("username"));
-            usuario.put("estado", resultSet.getString("estado"));
-            usuario.put("id_rol", resultSet.getString("id_rol"));
-            usuario.put("género", resultSet.getString("genero"));
-            usuario.put("teléfono", resultSet.getString("telefono"));
-            usuario.put("edad", resultSet.getString("edad"));
+            usuario.put("ID", resultSet.getString("id_usuario"));
+            usuario.put("Nombre", resultSet.getString("nombre"));
+            usuario.put("Apellido", resultSet.getString("apellido"));
+            usuario.put("Username", resultSet.getString("username"));
+            usuario.put("Estado", resultSet.getString("estado"));
+            usuario.put("ID_rol", resultSet.getString("id_rol"));
+            usuario.put("Género", resultSet.getString("genero"));
+            usuario.put("Teléfono", resultSet.getString("telefono"));
+            usuario.put("Edad", resultSet.getString("edad"));
 
             listaClientes.put(usuario);
         }
         return listaClientes;
     }
-
 
 }
---
+++ backend/API-paqueExpres/src/main/java/com/paqueExpres/controllers/user/PutuserServlet.java
@@ -0,0 +1,69 @@
+package com.paqueExpres.controllers.user;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import com.paqueExpres.DAO.UsuarioDAO;
+
+@WebServlet
+public class PutuserServlet extends HttpServlet {
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // obtener datos enviados
+        BufferedReader reader = request.getReader();
+
+        // instanciar conexion
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+        // responder con boolean
+        boolean estado;
+
+        // Respuesta JSON
+        JSONObject jsonResponse = new JSONObject();
+
+        try {
+
+            // establecer conecion
+            usuarioDAO.conectar();
+
+            estado = usuarioDAO.updateUser(reader);
+
+            // Verificar si se actualizó el cliente
+            if (estado) {
+                // El cliente se actualizó correctamente
+                jsonResponse.put("success", true);
+                jsonResponse.put("message", "Los datos del Usuario han sido actualizados exitosamente!");
+            } else {
+                // No se encontró el cliente para actualizar
+                jsonResponse.put("success", false);
+                jsonResponse.put("message", "El Usuario no existe.");
+            }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+            // manejamos erroes en la conexion de base de datos:
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write("Error al conectar a la DB: " + e.getMessage());
+
+        }
+
+        // Configurar la respuesta HTTP como JSON
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8"); // Configurar la codificación de caracteres
+
+    }
+}
