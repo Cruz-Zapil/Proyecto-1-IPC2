d83cfa947be089167d633f7ca0204b8f8deb1315
Cruz-Zapil <victorzapil202030851@cunoc.edu.gt>
2024-04-12 13:12:40 -0600

    actualizando commit de home

---
+++ frontend/src/app/operador/operador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operador',
+  standalone: true,
+  imports: [],
+  templateUrl: './operador.component.html',
+  styleUrl: './operador.component.css'
+})
+export class OperadorComponent {
+
+}
---
+++ frontend/src/app/recep/recep.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recep',
+  standalone: true,
+  imports: [],
+  templateUrl: './recep.component.html',
+  styleUrl: './recep.component.css'
+})
+export class RecepComponent {
+
+}
