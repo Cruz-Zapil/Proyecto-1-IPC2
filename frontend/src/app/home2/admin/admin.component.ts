3e33a748c47a77f24dcb5cc62d2702a06d49ab10
Cruz-Zapil <victorzapil202030851@cunoc.edu.gt>
2024-04-14 22:27:02 -0600

    actualizando dependecia

--- frontend/src/app/home2/admin/admin.component.ts
+++ frontend/src/app/home2/admin/admin.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NewUserComponent } from "./usuario/new-user/new-user.component";
 
 @Component({
-  selector: 'app-admin',
-  standalone: true,
-  imports: [],
-  templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+    selector: 'app-admin',
+    standalone: true,
+    templateUrl: './admin.component.html',
+    styleUrl: './admin.component.css',
+    imports: [ NewUserComponent]
 })
 export class AdminComponent {
 
